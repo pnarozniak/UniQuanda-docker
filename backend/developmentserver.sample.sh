11fb17138c78a2ba597f30ebff75c56fdc6583f3
xlegodroit <xxvege@gmail.com>
2022-10-23 18:17:18 +0200

    Added UniQuandaClient:Url to app settings

--- backend/developmentserver.sample.sh
+++ backend/developmentserver.sample.sh
@@ -13,6 +13,7 @@ dotnet user-secrets set "AWS:AccessKeyId" "xyz" --project "/app/UniQuanda.Presen
 dotnet user-secrets set "AWS:SecretAccessKey" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 dotnet user-secrets set "AWS:Region" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 dotnet user-secrets set "Image:EndpointUrl" "https://dev.uniquanda.pl:{TU_WSTAW_WARTOŚĆ_Z_ENV_BACKEND_HTTPS_PORT}/api/Image/" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
+dotnet user-secrets set "UniQuandaClient:Url" "https://dev.uniquanda.pl" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 
 dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AuthDbContext --connection "Host=Uniquanda_Authorization_Database;Port=5432;Database=uniquanda;Username=xyz;Password=xyz"
 dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AppDbContext --connection "Host=Uniquanda_DB;Port=5432;Database=uniquanda;Username=xyz;Password=xyz"
