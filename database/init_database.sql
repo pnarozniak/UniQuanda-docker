8d3543df58f8baf6b76d8ddbc290ea927bd247a2
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-10-28 13:01:32 +0200

    Dodanie mechanizmu fulltextsearch w języku polskim do baz danych

--- database/init_database.sql
+++ database/init_database.sql
@@ -1,1 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS uniquanda AUTHORIZATION uniquanda;
+BEGIN;
+      CREATE TEXT SEARCH CONFIGURATION public.polish ( COPY = pg_catalog.english );
+
+      CREATE TEXT SEARCH DICTIONARY polish_ispell (
+        TEMPLATE = ispell,
+        DictFile = polish,
+        AffFile = polish,
+        StopWords = polish
+      );
+
+      ALTER TEXT SEARCH CONFIGURATION polish
+        ALTER MAPPING FOR asciiword, asciihword, hword_asciipart, word, hword, hword_part
+        WITH polish_ispell;
+COMMIT;
