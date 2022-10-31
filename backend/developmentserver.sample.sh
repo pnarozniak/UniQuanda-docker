ea0cb25eb68c707bcfa2af1248e735fedd6a5798
xlegodroit <xxvege@gmail.com>
2022-10-31 13:58:10 +0100

    Removed verificationURL

--- backend/developmentserver.sample.sh
+++ backend/developmentserver.sample.sh
@@ -15,7 +15,6 @@ dotnet user-secrets set "AWS:Region" "xyz" --project "/app/UniQuanda.Presentatio
 dotnet user-secrets set "Image:EndpointUrl" "https://dev.uniquanda.pl:{TU_WSTAW_WARTOŚĆ_Z_ENV_BACKEND_HTTPS_PORT}/api/Image/" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 dotnet user-secrets set "UniQuandaClient:Url" "https://dev.uniquanda.pl" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 dotnet user-secrets set "Recaptcha:SecretKey" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
-dotnet user-secrets set "Recaptcha:VerificationApiUrl" "https://www.google.com/recaptcha/api/siteverify" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 
 dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AuthDbContext --connection "Host=Uniquanda_Authorization_Database;Port=5432;Database=uniquanda;Username=xyz;Password=xyz"
 dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AppDbContext --connection "Host=Uniquanda_DB;Port=5432;Database=uniquanda;Username=xyz;Password=xyz"
