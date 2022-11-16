7a7adc34082b05041510672fe71f2261858bbc2d
Dmitry "Ventan" Teplov <56700157+Ventan00@users.noreply.github.com>
2022-11-16 17:19:06 +0100

    Feature/quan 155/lista tagów/fulltext search (#11)
    * Dodanie mechanizmu fulltextsearch w języku polskim do baz danych
    
    * Dodano extensions pozwalająca wykrywać teksty z błędem
    
    * Zmodyfikowano skrypt dodawania fulltext searcha oraz dodano inicjalizację powtarzalnych bytów
    
    * poprawki
    
    * Usunięto SQL z dockera
    Dodano auto-migrację w środowisku lokalnym
    Dodano import serwerów pgadmina po rebuildzie
    
    * Usunięcie niezauważonych plików
    
    * Aktualizacja instrukcji

---
+++ backend/local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AuthDbContext --connection "Host=Uniquanda_Authorization_Database;Port=5432;Database=uniquanda;Username=uniquanda;Password=uniquanda"
+dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AppDbContext --connection "Host=Uniquanda_DB;Port=5432;Database=uniquanda;Username=uniquanda;Password=uniquanda"
+dotnet watch run --urls="http://+:80;https://+:443" --project UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj
--- database/init_database.sql
+++ database/init_database.sql
@@ -1,1 +1,16 @@
 CREATE SCHEMA IF NOT EXISTS uniquanda AUTHORIZATION uniquanda;
+SET search_path = uniquanda;
+BEGIN;
+      CREATE TEXT SEARCH CONFIGURATION uniquanda.polish ( COPY = pg_catalog.english );
+
+      CREATE TEXT SEARCH DICTIONARY uniquanda.polish_ispell (
+        TEMPLATE = ispell,
+        DictFile = polish,
+        AffFile = polish,
+        StopWords = polish
+      );
+
+      ALTER TEXT SEARCH CONFIGURATION uniquanda.polish
+        ALTER MAPPING FOR asciiword, asciihword, hword_asciipart, word, hword, hword_part
+        WITH polish_ispell;
+COMMIT;
