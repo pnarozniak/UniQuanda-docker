08a78c43ce283cdda8fae0ca71d1f0ee5e7b47b2
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-12-06 17:38:31 +0100

    Zmieniono loadbalancer na nginx i dodano niezależne konta AWS dla backendu

--- backend/local.sh
+++ backend/local.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+dotnet user-secrets init --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj
+    dotnet user-secrets set "AWS:Region" "eu-central-1" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 if [ $1 -eq 1 ]
 then
+    dotnet user-secrets set "AWS:AccessKeyId" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
+    dotnet user-secrets set "AWS:SecretAccessKey" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
     dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AuthDbContext --connection "Host=Uniquanda_Authorization_Database;Port=5432;Database=uniquanda;Username=uniquanda;Password=uniquanda"
     dotnet-ef database update --project ./../app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj --context AppDbContext --connection "Host=Uniquanda_DB;Port=5432;Database=uniquanda;Username=uniquanda;Password=uniquanda"
+else
+    dotnet user-secrets set "AWS:AccessKeyId" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
+    dotnet user-secrets set "AWS:SecretAccessKey" "xyz" --project "/app/UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj"
 fi
 
 dotnet watch run --urls="http://+:80;https://+:443" --project UniQuanda.Presentation.API/UniQuanda.Presentation.API.csproj
